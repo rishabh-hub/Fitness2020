e48ae176c92eb5c5e2b0c93be98cf871f9005ef6
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-05-15 21:47:21 +0530

    new activity has been added

---
+++ app/src/main/java/com/example/fitness2020/fragments/TrialCardActivity.java
@@ -0,0 +1,18 @@
+package com.example.fitness2020.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fitness2020.R;
+
+public class TrialCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trial_card);
+
+
+    }
+}
