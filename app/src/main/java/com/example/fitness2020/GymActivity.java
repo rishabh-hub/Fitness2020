f389f63759d7339b006f13694e8e2bd89f97fc00
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-05-21 20:49:30 +0530

    java classes for the tabs added

---
+++ app/src/main/java/com/example/fitness2020/GymActivity.java
@@ -0,0 +1,86 @@
+package com.example.fitness2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.fitness2020.Adapters.GymPageAdapter;
+import com.example.fitness2020.fragments.GymDescriptionFragment;
+import com.example.fitness2020.fragments.GymOfferingsFragment;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class GymActivity extends AppCompatActivity {
+
+    TabLayout gymTabLayout;
+    TabItem descriptionTab,offeringsTab;
+    ViewPager gymViewPager;
+    GymPageAdapter gymPageAdapter;
+    Fragment gymFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gym);
+
+        attachId();
+
+        gymPageAdapter=new GymPageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,gymTabLayout.getTabCount());
+
+        gymViewPager.setAdapter(gymPageAdapter);
+
+        gymTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                gymViewPager.setCurrentItem(tab.getPosition());
+
+
+                switch(tab.getPosition())
+                {
+                    case 0: gymFragment=new GymDescriptionFragment();
+                        loadFragment(gymFragment);
+                        break;
+                    case 1: gymFragment=new GymOfferingsFragment();
+                        loadFragment(gymFragment);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        gymViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(gymTabLayout));
+
+    }
+
+    void attachId()
+    {
+        gymTabLayout=findViewById(R.id.gym_tab_layout);
+        descriptionTab=findViewById(R.id.gym_description);
+        offeringsTab=findViewById(R.id.gym_offerings);
+        gymViewPager=findViewById(R.id.gym_view_pager);
+
+    }
+
+    void loadFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.gym_frame_layout,fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/fitness2020/fragments/GymDescriptionFragment.java
@@ -0,0 +1,35 @@
+package com.example.fitness2020.fragments;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GymDescriptionFragment extends Fragment {
+
+    View view;
+    Context context;
+
+    public GymDescriptionFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //inflatefragment  view=inflater.inflate()
+
+        return view;
+    }
+}
