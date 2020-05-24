07073077451f74b35ae78af67966171381762c35
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-05-24 20:45:08 +0530

    slider xmls and signupnew activity created

---
+++ app/src/main/java/com/example/fitness2020/SignupNewActivity.java
@@ -0,0 +1,21 @@
+package com.example.fitness2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class SignupNewActivity extends AppCompatActivity {
+
+    ViewPager sliderDotsVp;
+    LinearLayout signupDotsLinlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_new);
+
+
+    }
+}
