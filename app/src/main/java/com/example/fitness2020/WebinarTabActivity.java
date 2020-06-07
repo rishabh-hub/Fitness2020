ade3df0cd80a0d8c5a7e1fed6d59e7517dc6d31d
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-06-07 20:21:24 +0530

    webinar activity added

---
+++ app/src/main/java/com/example/fitness2020/WebinarTabActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitness2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebinarTabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webinar_tab);
+    }
+}
