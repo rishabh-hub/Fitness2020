255eda2a9fac66e04ade97e20927a13ce0848393
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-06-10 21:21:49 +0530

    video java layout set

--- app/src/main/java/com/example/fitness2020/VideosActivity.java
+++ app/src/main/java/com/example/fitness2020/VideosActivity.java
@@ -1,14 +1,62 @@
 package com.example.fitness2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.fitness2020.Models.VideoModel;
+
+import java.util.ArrayList;
+
 public class VideosActivity extends AppCompatActivity {
+    ArrayList<VideoModel> fitnessPassVideos = new ArrayList<>();
+    ArrayList<VideoModel> dailyWorkoutVideos = new ArrayList<>();
+    ArrayList<VideoModel> studioVideos = new ArrayList<>();
+    ArrayList<VideoModel> corporateVideos = new ArrayList<>();
+    RecyclerView passRV,workoutRV,studioRV,corporateRV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
+        attachID();
+        addDummyData();
+        setLayoutManagers();
+        initAdapter();
+        setAdapters();
+
+    }
+
+    private void addDummyData() {
+        for (int i=0;i<6;i++)
+        {
+//            fitnessPassVideos.add();
+//            dailyWorkoutVideos.add();
+//            studioVideos.add();
+//            corporateVideos.add();
+        }
+    }
+
+    private void setAdapters() {
+    }
+
+    private void initAdapter() {
+    }
+
+    private void setLayoutManagers() {
+        passRV.setLayoutManager(new LinearLayoutManager(VideosActivity.this,LinearLayoutManager.HORIZONTAL,false));
+        workoutRV.setLayoutManager(new LinearLayoutManager(VideosActivity.this,LinearLayoutManager.HORIZONTAL,false));
+        studioRV.setLayoutManager(new LinearLayoutManager(VideosActivity.this,LinearLayoutManager.HORIZONTAL,false));
+        corporateRV.setLayoutManager(new LinearLayoutManager(VideosActivity.this,LinearLayoutManager.HORIZONTAL,false));
+    }
+
+    private void attachID() {
+//        passRV = findViewById();
+//        workoutRV = findViewById();
+//        studioRV = findViewById();
+//        corporateRV = findViewById();
     }
 }
