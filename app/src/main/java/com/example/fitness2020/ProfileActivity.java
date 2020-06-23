cfb2293116d4438d4cb804e1537369e8a76e2577
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-06-23 23:18:50 +0530

    > spineer added in profile activity

--- app/src/main/java/com/example/fitness2020/ProfileActivity.java
+++ app/src/main/java/com/example/fitness2020/ProfileActivity.java
@@ -2,22 +2,36 @@ package com.example.fitness2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
-    ImageView backButton,profileImage;
-    TextView logoutButton,profileName;
-    EditText emailId,phoneNo,dateOfBirth;
-    CheckBox male,female;
+public class ProfileActivity extends AppCompatActivity implements  AdapterView.OnItemSelectedListener {
+
+    ImageView backButton;
+    TextView logoutButton;
+    EditText emailId,phoneNo,profileName,anniversary,dateOfBirth,age;
+    Spinner gender;
     Button updateButton;
+
+    Calendar calendar=Calendar.getInstance();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,51 +40,106 @@ public class ProfileActivity extends AppCompatActivity {
 
         attachId();
 
-//        backButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
-//
-//        logoutButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //logout of the current user account
-//            }
-//        });
-//
-//        updateButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //update the details and return to the previous screen
-//
-//                Toast.makeText(ProfileActivity.this, "Updated", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//        profileImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //get image from either gallery or the camera directly
-//            }
-//        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-        //also check how to disable check on the other gender if one gender is selected
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //logout of the current user account
+            }
+        });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update the details and return to the previous screen
+
+                Toast.makeText(ProfileActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        dateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(ProfileActivity.this, date, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        populateGenderSpinner();
+        gender.setOnItemSelectedListener(this);
+
+//        also check how to disable check on the other gender if one gender is selected
 
     }
 
     void attachId()
     {
-//        backButton=findViewById(R.id.profile_page_back_btn);
-//        profileImage=findViewById(R.id.profile_page_profile_image);
-//        logoutButton=findViewById(R.id.profile_page_logout_tv);
-//        profileName=findViewById(R.id.profile_page_profile_name);
-//        emailId=findViewById(R.id.profile_page_email_et);
-//        phoneNo=findViewById(R.id.profile_page_number_et);
-//        dateOfBirth=findViewById(R.id.profile_page_dob_et);
-//        male=findViewById(R.id.profile_page_male_cb);
-//        female=findViewById(R.id.profile_page_female_cb);
-//        updateButton=findViewById(R.id.profile_page_update_btn);
+        backButton=findViewById(R.id.profile_page_back_btn);
+        logoutButton=findViewById(R.id.profile_page_logout_tv);
+        emailId=findViewById(R.id.profile_page_email_et);
+        phoneNo=findViewById(R.id.profile_page_mobile_et);
+        profileName=findViewById(R.id.profile_page_name_et);
+        anniversary=findViewById(R.id.profile_page_anniversary_et);
+        dateOfBirth=findViewById(R.id.profile_page_bday_et);
+        age=findViewById(R.id.profile_page_age_et);
+        gender=findViewById(R.id.gender_spinner);
+        updateButton=findViewById(R.id.profile_update_button);
+
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dateOfBirth.setText(sdf.format(calendar.getTime()));
+    }
+
+    void populateGenderSpinner()
+    {
+        ArrayList<String> genderData=new ArrayList<>();
+        genderData.add("Male");
+        genderData.add("Female");
+        genderData.add("Custom");
+        genderData.add("Prefer not to say");
+        ArrayAdapter<String> genderAdapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,genderData);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gender.setAdapter(genderAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        if(parent.getId()==R.id.gender_spinner)
+        {
+            Toast.makeText(this, "Selected "+parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
