e4808deaf182a9dbc863c372af6ff1ff61df7be2
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-07-02 19:32:42 +0530

    video internal page added

---
+++ app/src/main/java/com/example/fitness2020/VideoInternalPage.java
@@ -0,0 +1,14 @@
+package com.example.fitness2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoInternalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_internal_page);
+    }
+}
