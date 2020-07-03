2b5ef5bd2297717905a4f16c3488f1b121defbf7
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-07-03 23:27:29 +0530

    > editText on the TrialMembershipBooking.java page has been corrected

--- app/src/main/java/com/example/fitness2020/TrialMembershipBooking.java
+++ app/src/main/java/com/example/fitness2020/TrialMembershipBooking.java
@@ -2,13 +2,21 @@ package com.example.fitness2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class TrialMembershipBooking extends AppCompatActivity {
 
     ImageView backButton;
+    TextInputLayout promo;
+    EditText promoCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +30,26 @@ public class TrialMembershipBooking extends AppCompatActivity {
                 finish();
             }
         });
+        promo.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promo.clearFocus();
+                promoCode.clearFocus();
+                promoCode.setText("");
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(promo.getWindowToken(), 0);
+
+            }
+        });
+
     }
 
     private void attachId() {
 
         backButton=findViewById(R.id.membership_booking_page_back_btn);
+        promo=findViewById(R.id.promo_layout);
+        promoCode=findViewById(R.id.final_booking_page_gym_promoET);
+
     }
 
 }
