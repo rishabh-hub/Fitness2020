5725650b74e218ea691779a221a3c0a9778cb3d1
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-07-08 21:37:22 +0530

    > Razorpay has been integrated

--- app/src/main/java/com/example/fitness2020/TrialMembershipBooking.java
+++ app/src/main/java/com/example/fitness2020/TrialMembershipBooking.java
@@ -2,20 +2,31 @@ package com.example.fitness2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-public class TrialMembershipBooking extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class TrialMembershipBooking extends AppCompatActivity implements PaymentResultListener {
 
     ImageView backButton;
     TextInputLayout promo;
     EditText promoCode;
+    Button payButton;
+    TextView finalAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,13 @@ public class TrialMembershipBooking extends AppCompatActivity {
             }
         });
 
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPayment();
+            }
+        });
+
     }
 
     private void attachId() {
@@ -49,7 +67,62 @@ public class TrialMembershipBooking extends AppCompatActivity {
         backButton=findViewById(R.id.membership_booking_page_back_btn);
         promo=findViewById(R.id.promo_layout);
         promoCode=findViewById(R.id.final_booking_page_gym_promoET);
+        payButton=findViewById(R.id.membership_final_book_btn);
+        finalAmount=findViewById(R.id.pass_total_amount);
+    }
+    public void startPayment() {
+        /**
+         * You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Fitness");
+            options.put("description", "App Payment");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://rzp-mobile.s3.amazonaws.com/images/rzp.png");
+            options.put("currency", "INR");
+            String payment = finalAmount.getText().toString();
+            // amount is in paise so please multiple it by 100
+            //Payment failed Invalid amount (should be passed in integer paise. Minimum value is 100 paise, i.e. ₹ 1)
+            double total = Double.parseDouble(payment);
+            total = total * 100;
+            options.put("amount", total);
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "rishabh.1056@gmail.com");
+            preFill.put("contact", "887751441");
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        // payment successfull pay_DGU19rDsInjcF2
+//        Log.e(TAG, " payment successfull "+ s.toString());
+        Toast.makeText(this, "Payment successfully done! " +s, Toast.LENGTH_SHORT).show();
 
     }
 
+    @Override
+    public void onPaymentError(int i, String s) {
+//        Log.e(TAG,  "error code "+String.valueOf(i)+" -- Payment failed "+s.toString()  );
+        try {
+            Toast.makeText(this, "Payment error please try again", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e("OnPaymentError", "Exception in onPaymentError", e);
+        }
+
+    }
 }
+
+
