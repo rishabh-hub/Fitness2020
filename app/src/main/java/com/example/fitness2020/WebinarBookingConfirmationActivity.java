479712b21fbe1be5765a74194f18ee263b33ebc2
Rishabh Singh <rishabh.1056@gmail.com>
2020-07-19 20:43:49 +0530

    > Deep Linking

--- app/src/main/java/com/example/fitness2020/WebinarBookingConfirmationActivity.java
+++ app/src/main/java/com/example/fitness2020/WebinarBookingConfirmationActivity.java
@@ -33,7 +33,9 @@ public class WebinarBookingConfirmationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Uri uri=Uri.parse("https://www.instagram.com/oauth/authorize?client_id=1486092204916874&redirect_uri=https://httpstat.us/200&scope=user_profile,user_media&response_type=code");
+//                Uri uri=Uri.parse("https://www.instagram.com/oauth/authorize?client_id=1486092204916874&redirect_uri=https://httpstat.us/200&scope=user_profile,user_media&response_type=code");
+
+                Uri uri=Uri.parse("http://www.exampleabc.com");
                 Intent instaIntent=new Intent(Intent.ACTION_VIEW,uri);
 
                 startActivity(instaIntent);
